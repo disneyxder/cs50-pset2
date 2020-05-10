57b62cccf49a87326baa304e6b76d8d6deeb5b5e
disneyxder <58625037+disneyxder@users.noreply.github.com>
2020-05-10 15:58:36 +0800

    Update readability.c

--- readability.c
+++ readability.c
@@ -1,57 +1,57 @@
 #include <cs50.h>
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 #include <math.h>
 
 int main(void)
 {
-
-    string input = get_string("Text: ");
-
-    // counting letters, words & sentences
-    float letters = 0, words = 0, sentences = 0;
-    for (int i = 0, n = strlen(input); i < n; i++)
+    // variables
+    string s = get_string("Text: ");
+    int letter, word, sentence; 
+    letter = 0;
+    word = 1; 
+    sentence = 0; 
+    
+    // count the letters, words and sentences
+    for (int i = 0; i < strlen(s); i++)
     {
-        if (isalpha(input[i]))
+        if (isalpha(s[i]))
         {
-            letters++;
+            letter++; 
         }
-        if ((isalpha(input[i]) && isspace(input[i + 1])) || (isalpha(input[i]) && ispunct(input[i + 1])) || (isalpha(input[i]) &&
-                input[i + 1] == '\0'))
+        if (s[i] == ' ' && isalpha(s[i+1]))
         {
-            words++;
+            word++;
         }
-        if (input[i] == '.' || input[i] == '!' || input[i] == '?')
+        if (s[i] == '.' || s[i] == '!' || s[i] == '?')
         {
-            sentences++;
-        }
-        if (isalpha(input[i]) && ispunct(input[i + 1]) && isalpha(input[i + 2]))
-        {
-            words--;
+            sentence++;
         }
     }
-
-    // average number of letters || sentences per 100 words
-    float L = letters / words * 100;
-    float S = sentences / words * 100;
-
+    
+    // calculate index of text
+    float L, S, indexf; 
+    L = letter / word * 100; 
+    S = sentence / word * 100; 
+    indexf = 0.0588 * L - 0.296 * S - 15.8;
+    int index; 
+    index = round(indexf);
+    
     // print grade
-    float index = 0.0588 * L - 0.296 * S - 15.8;
-    int index1 = round(index);
-    if (index1 >= 1 && index1 <= 16)
+    if (index >= 1 && index < 16)
     {
-        printf("Grade %i\n", index1);
+        printf("Grade %i\n", index);
+        return 0; 
     }
-    else
+    if (index < 1)
     {
-        if (index1 < 1)
-        {
-            printf("Before Grade 1\n");
-        }
-        if (index1 > 16)
-        {
-            printf("Grade 16+\n");
-        }
+        printf("Before Grade 1\n");
+        return 0; 
+    }
+    if (index >= 16)
+    {
+        printf("Grade 16+\n");
+        return 0; 
     }
 }
